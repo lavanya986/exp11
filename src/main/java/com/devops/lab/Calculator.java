1441bf8fa7af6d6402f49cf60f991928249b2a66
lavanya986 <lavanyam986@gmail.com>
2026-04-20 14:06:16 +0530

    Add files via upload

---
+++ src/main/java/com/devops/lab/Calculator.java
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+package com.devops.lab;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+=======
+package com.devops.lab;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+>>>>>>> 7cad21321509551ac86909866cdd9a6f3f79881e
+}
